883b19c45f9128fd3bc5865956604d93cc0e19b2
FlexCraft112 <159067201+FlexCraft112@users.noreply.github.com>
2026-01-18 21:16:41 +0100

    Update AutoFireworkRegions.java

--- src/main/java/me/flexcraft/autofirework/AutoFireworkRegions.java
+++ src/main/java/me/flexcraft/autofirework/AutoFireworkRegions.java
@@ -1,16 +1,13 @@
 package me.flexcraft.autofirework;
 
-import com.sk89q.worldedit.bukkit.BukkitAdapter;
-import com.sk89q.worldguard.WorldGuard;
-import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
-import com.sk89q.worldguard.protection.managers.RegionManager;
-import com.sk89q.worldguard.protection.regions.ProtectedRegion;
 import org.bukkit.*;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Firework;
 import org.bukkit.inventory.meta.FireworkMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
@@ -21,101 +18,64 @@ public class AutoFireworkRegions extends JavaPlugin {
     @Override
     public void onEnable() {
         saveDefaultConfig();
-        startFireworkTask();
+        startFireworksTask();
         getLogger().info("AutoFireworkRegions enabled");
     }
 
-    private void startFireworkTask() {
-        int interval = getConfig().getInt("interval-seconds", 10);
+    private void startFireworksTask() {
+        FileConfiguration cfg = getConfig();
+
+        int interval = cfg.getInt("interval-seconds", 10);
+        List<String> regions = cfg.getStringList("regions");
+        int power = cfg.getInt("firework.power", 2);
+        List<String> colorNames = cfg.getStringList("firework.colors");
+
+        List<Color> colors = new ArrayList<>();
+        for (String name : colorNames) {
+            try {
+                colors.add(DyeColor.valueOf(name.toUpperCase()).getColor());
+            } catch (IllegalArgumentException ex) {
+                getLogger().warning("Invalid color in config: " + name);
+            }
+        }
+
+        if (colors.isEmpty()) {
+            colors.add(Color.WHITE);
+        }
 
         new BukkitRunnable() {
             @Override
             public void run() {
-                spawnFireworks();
+                for (World world : Bukkit.getWorlds()) {
+                    for (String regionName : regions) {
+                        spawnFireworkInRegion(world, regionName, colors, power);
+                    }
+                }
             }
         }.runTaskTimer(this, 20L, interval * 20L);
     }
 
-    private void spawnFireworks() {
-        WorldGuardPlugin wg = getWorldGuard();
-        if (wg == null) return;
+    private void spawnFireworkInRegion(World world, String regionName, List<Color> colors, int power) {
+        // ⚠ Пока заглушка — дальше можно подключить WorldGuard
+        Location loc = world.getSpawnLocation().clone().add(
+                random.nextInt(10) - 5,
+                random.nextInt(5) + 2,
+                random.nextInt(10) - 5
+        );
 
-        List<String> regionNames = getConfig().getStringList("regions");
-        if (regionNames.isEmpty()) return;
-
-        for (World world : Bukkit.getWorlds()) {
-            RegionManager manager = WorldGuard.getInstance()
-                    .getPlatform()
-                    .getRegionContainer()
-                    .get(BukkitAdapter.adapt(world));
-
-            if (manager == null) continue;
-
-            for (String regionName : regionNames) {
-                ProtectedRegion region = manager.getRegion(regionName);
-                if (region == null) continue;
-
-                Location loc = getRandomLocationInRegion(world, region);
-                if (loc != null) {
-                    spawnFirework(loc);
-                }
-            }
-        }
-    }
-
-    private Location getRandomLocationInRegion(World world, ProtectedRegion region) {
-        int minX = region.getMinimumPoint().getBlockX();
-        int minY = region.getMinimumPoint().getBlockY();
-        int minZ = region.getMinimumPoint().getBlockZ();
-
-        int maxX = region.getMaximumPoint().getBlockX();
-        int maxY = region.getMaximumPoint().getBlockY();
-        int maxZ = region.getMaximumPoint().getBlockZ();
-
-        for (int i = 0; i < 10; i++) {
-            int x = random.nextInt(maxX - minX + 1) + minX;
-            int z = random.nextInt(maxZ - minZ + 1) + minZ;
-            int y = world.getHighestBlockYAt(x, z) + 1;
-
-            Location loc = new Location(world, x + 0.5, y, z + 0.5);
-            if (region.contains(
-                    BukkitAdapter.asBlockVector(loc)
-            )) {
-                return loc;
-            }
-        }
-        return null;
-    }
-
-    private void spawnFirework(Location location) {
-        Firework firework = location.getWorld().spawn(location, Firework.class);
-        FireworkMeta meta = firework.getFireworkMeta();
-
-        FireworkEffect.Type type = FireworkEffect.Type.values()
-                [random.nextInt(FireworkEffect.Type.values().length)];
-
-        List<String> colorsCfg = getConfig().getStringList("firework.colors");
-        Color c1 = Color.fromRGB(random.nextInt(255), random.nextInt(255), random.nextInt(255));
-
-        if (!colorsCfg.isEmpty()) {
-            try {
-                c1 = Color.valueOf(colorsCfg.get(random.nextInt(colorsCfg.size())));
-            } catch (Exception ignored) {}
-        }
+        Firework fw = world.spawn(loc, Firework.class);
+        FireworkMeta meta = fw.getFireworkMeta();
 
         FireworkEffect effect = FireworkEffect.builder()
-                .with(type)
-                .withColor(c1)
+                .with(FireworkEffect.Type.BALL_LARGE)
+                .withColor(colors.get(random.nextInt(colors.size())))
+                .withFade(Color.WHITE)
                 .flicker(true)
                 .trail(true)
                 .build();
 
         meta.addEffect(effect);
-        meta.setPower(getConfig().getInt("firework.power", 2));
-        firework.setFireworkMeta(meta);
-    }
-
-    private WorldGuardPlugin getWorldGuard() {
-        return (WorldGuardPlugin) Bukkit.getPluginManager().getPlugin("WorldGuard");
+        meta.setPower(power);
+        fw.setFireworkMeta(meta);
     }
 }
